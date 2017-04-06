3fae57ef55739175b49ff758195390a952569fbc
Cory Shain <osu8358@owens-rw01.ten.osc.edu>
2017-04-06 15:52:29 -0400

    updates to grid search script

--- scripts/gridSearchZerospeechTest.py
+++ scripts/gridSearchZerospeechTest.py
@@ -1,22 +1,28 @@
 import os
 
+run = False
+
 if __name__ == "__main__":
     preamble = file("scripts/runNN.sh").read()
 
-    for wSize in [40]:
+    for wSize in [20, 80]:
         for uSize in [400]:
-            for sSize in [500, 1000, 1500]:
-                for wDrop in [.25]:
-                    for cDrop in [.25]:
-                        for maxLen in [50, 100]:
-                            for maxChar in [250, 500]:
-                                for batchSize in [500,1000,5000]:
-                                    rName = "noisy-w%d-u%d-s%d-wd%g-c%g-l%d-C%d-b%d" % (wSize, uSize, 
-                                                                       sSize, wDrop, cDrop, maxLen, maxChar, batchSize)
-                                    cmd = "python scripts/autoencodeDecodeChars.py build/zerospeech/test/ --acoustic --segfile data/test_vad.txt --goldfile data/test.phn --gpufrac None --wordHidden %d --uttHidden %d --segHidden %d --wordDropout %g --charDropout %g --maxLen %d --maxChar %d --batchSize %d --logfile %s/" % (wSize, uSize, sSize, wDrop, cDrop, maxLen, maxChar, batchSize, rName)
+            for sSize in [1500]:
+                for wDrop in [0, .25]:
+                    for cDrop in [0, .25]:
+                        for maxLen in [100]:
+                            for maxChar in [500]:
+                                for nSamples in [100]:
+                                    for meanWLen in [10,30]:
+                                        for batchSize in [1000,5000]:
+                                            maxUtt = maxChar/meanWLen
+                                            rName = "noisy-w%d-u%d-s%d-wd%g-c%g-l%d-C%d-U%d-b%d" % (wSize, uSize, 
+                                                                               sSize, wDrop, cDrop, maxLen, maxChar, maxUtt, batchSize)
+                                            cmd = "python scripts/autoencodeDecodeChars.py build/zerospeech/test/ --acoustic --segfile data/test_vad.txt --goldfile data/test.phn --gpufrac None --wordHidden %d --uttHidden %d --segHidden %d --wordDropout %g --charDropout %g --maxLen %d --maxChar %d --maxUtt %d --batchSize %d --logfile %s/" % (wSize, uSize, sSize, wDrop, cDrop, maxLen, maxChar, maxUtt, batchSize, rName)
 
-                                    outf = file("batch/run%s.sh" % rName, "w")
-                                    print >>outf, preamble + "\n" + cmd
-                                    outf.close()
-                                    os.system("qsub batch/run%s.sh" %rName)
-                                    
+                                            outf = file("batch/run%s.sh" % rName, "w")
+                                            print >>outf, preamble + "\n" + cmd
+                                            outf.close()
+                                            if run:
+                                                os.system("qsub batch/run%s.sh" %rName)
+                                            
